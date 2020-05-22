83ee9411d0d68704876226a549fdaf72ab8ae3af
Nicholas Zaleski <nicholas.zaleski1@gmail.com>
2020-05-21 21:54:12 -0400

    WIP

---
+++ pages/food.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import 
+
+const food = () => {
+    const [name, setName] = useState('')
+    const [genre, setGenre] = useState('')
+    const [authorId, setAuthorId] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log({authorId, genre, name})
+    }
+    return (
+        <div>
+            <form id="add-book" onSubmit={handleSubmit}>
+            <div className="field">
+                <label>Book Name</label>
+                <input type="text" value={name} onChange={e=> setName(e.target.value)}/>
+            </div>
+            <div className="field">
+                <label>Genre</label>
+                <input type="text" value={genre} onChange={e=> setGenre(e.target.value)}/>
+            </div>
+            <div className="field">
+                <label>Author</label>
+                <select value={authorId} onChange={e=> setAuthorId(e.target.value)}>
+                    
+                </select>
+                <button>+</button>
+            </div>
+
+        </form>
+        </div>
+    )
+}
+
+export default food
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import App from '../App'
 const index = () => {
     return (
         <div>
